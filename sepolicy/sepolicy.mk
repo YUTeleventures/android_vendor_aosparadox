d006841fb701e941f83ffd5904798ae176665c91
Lars Greiss <kufikugel@googlemail.com>
2015-09-01 02:02:34 +0530

    vendor: several selinux updates

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -1,11 +1,16 @@
 #
 # This policy configuration will be used by all products that
-# inherit from SSHD
 #
 
 BOARD_SEPOLICY_DIRS += \
     vendor/sshd/sepolicy
 
 BOARD_SEPOLICY_UNION += \
+    file.te \
     file_contexts \
+    fs_use \
+    genfs_contexts \
+    installd.te \
+    seapp_contexts \
+    vold.te \
     mac_permissions.xml
