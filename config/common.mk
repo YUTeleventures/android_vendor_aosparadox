namdeptrai2003/android_vendor_dnd

add userinit script(s) support
Allow user to provide userinit.sh and/or userinit.d/*
scripts under /data/local to be run on each boot.

Change-Id: I96f3034bcdea8d4cc75929d21b69efa31c1cb108

config/common.mk
@@ -40,6 +40,10 @@ PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
     vendor/cm/prebuilt/common/bin/sysinit:system/bin/sysinit
 
+# userinit support
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
+
 # Compcache/Zram support
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/etc/init.local.rc:system/etc/init.local.rc \

YUTeleventures/android_vendor_aosparadox

config/common.mk
@@ -49,11 +49,16 @@ endif
 PRODUCT_COPY_FILES += \
     vendor/sshd/prebuilt/common/bin/otasigcheck.sh:system/bin/otasigcheck.sh
 
+# userinit support
+PRODUCT_COPY_FILES += \
+    vendor/sshd/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
+	
 # init.d support
 PRODUCT_COPY_FILES += \
 	vendor/sshd/prebuilt/common/bin/sysinit:system/bin/sysinit \
 	vendor/sshd/prebuilt/common/etc/init.d/00sshd:system/etc/init.d/00sshd
-	
+
+# Bring in camera effects	
 PRODUCT_COPY_FILES +=  \
     vendor/sshd/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
     vendor/sshd/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd