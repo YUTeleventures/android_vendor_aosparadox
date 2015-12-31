cddc9d8c0fd006662dbfd8384c3ee923dfa5faaa
AudioGod <audiogod@sonic-developers.com>
2015-12-31 05:36:42 +0000

    Enable Stagefright FFMPEG plugin
    Change-Id: I6b213120424a3bec994bc8df8386f7e3f27bbfba
    Signed-off-by: AudioGod <audiogod@sonic-developers.com>

--- config/common.mk
+++ config/common.mk
@@ -151,14 +151,14 @@ PRODUCT_PACKAGES += \
 endif
 	
 # Stagefright FFMPEG plugin
-#PRODUCT_PACKAGES += \
-    #libffmpeg_extractor \
-    #libffmpeg_omx \
-    #media_codecs_ffmpeg.xml
+PRODUCT_PACKAGES += \
+    libffmpeg_extractor \
+    libffmpeg_omx \
+    media_codecs_ffmpeg.xml
 
-#PRODUCT_PROPERTY_OVERRIDES += \
-    #media.sf.omx-plugin=libffmpeg_omx.so \
-    #media.sf.extractor-plugin=libffmpeg_extractor.so
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.sf.omx-plugin=libffmpeg_omx.so \
+    media.sf.extractor-plugin=libffmpeg_extractor.so
 	
 # SSHD overlays
 PRODUCT_PACKAGE_OVERLAYS := vendor/sshd/overlay/common
